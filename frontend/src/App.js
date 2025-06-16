56371f9e35722fcd7fdd9ccf0004631c54c0db17
miranicad <tamira.leber@students.fhnw.ch>
2025-06-16 21:15:28 +0200

    add frontend to upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const [response, setResponse] = useState("");
+
   return (
-    <div>
-        <h2>Test</h2>
+    <div className='App-header'>
+        <h1>Test</h1>
+        <p>Here you can upload your picture</p>
+        <button>
+          <label htmlFor="file-upload" className="custom-file-upload">
+            Upload Picture
+          </label>
+          <input id="file-upload" type="file" accept="image/*" style={{ display: 'none' }} />
+        </button>
 
     </div>
   );
